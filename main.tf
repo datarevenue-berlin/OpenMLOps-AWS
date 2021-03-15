ba7cd76fe385978a97917290a94a4cee02d5c6bb
Thales <tvinagre@gmail.com>
2021-03-15 16:51:01 -0300

    Remove S3 config

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "eks-mlops-tf-state"
-    key = "terraform-dev.tfstate"
-    region = "eu-west-1"
-  }
-}
-
 provider "aws" {
   region = var.aws_region
 }
